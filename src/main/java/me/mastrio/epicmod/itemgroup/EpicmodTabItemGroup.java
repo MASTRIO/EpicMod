0655f3f3c028c655a19d18127e7ab6b879440109
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2021-05-06 12:28:11 +1000

    base files yay

---
+++ src/main/java/me/mastrio/epicmod/itemgroup/EpicmodTabItemGroup.java
@@ -0,0 +1,35 @@
+
+package me.mastrio.epicmod.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Blocks;
+
+import me.mastrio.epicmod.EpicmodModElements;
+
+@EpicmodModElements.ModElement.Tag
+public class EpicmodTabItemGroup extends EpicmodModElements.ModElement {
+	public EpicmodTabItemGroup(EpicmodModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabepicmod_tab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(Blocks.DIAMOND_BLOCK, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
